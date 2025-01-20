5a1438a99b587f8361060209bd0459f8d18a800c
hongwei <171832340@qq.com>
2025-01-20 10:24:26 +0800

    ci: 构建配置

--- packages/injectApiTypesWebpack/tsup.config.ts
+++ packages/injectApiTypesWebpack/tsup.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-    target: "node20",
-    entry: ['src/index.ts'],
-    outDir: 'dist',
-    clean: true,
-    format: ['cjs'],
-    // dts: true,
-    treeshake: true,
-    sourcemap: true
+export default defineConfig((options) => {
+    const devMode = process.env.NODE_ENV === 'development'
+    return {
+        target: "node16",
+        entry: ['src/index.ts'],
+        outDir: 'dist',
+        clean: true,
+        format: ['cjs'],
+        dts: true,
+        treeshake: true,
+        sourcemap: devMode ? 'inline' : false,
+    }
 })
