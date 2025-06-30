5bc4c6b361dcb8b13afb42ef8ff7567f6111b333
hongwei <171832340@qq.com>
2025-06-30 09:03:54 +0800

    feat(monitor-sdk): 添加自定义页面加载性能插件以增强数据上报

---
+++ packages/monitor-sdk/src/plugins/pageview.ts
@@ -0,0 +1,29 @@
+import { reportEvent } from '../core'
+import useApp from '../hooks/useApp'
+import { getOrigin, queryTransform2UrlParams } from '../utils'
+
+/**
+ * 自定义页面加载性能插件保证更多的数据上报
+ */
+
+export default {
+  plugin_name: 'pageview',
+  init(sd: any) {
+    sd.ee.sdk.on('ready', () => {
+        collectPageView()
+    })
+  },
+}
+
+export function collectPageView() {
+  const router = useApp().$router
+  if(!router) return
+  router.afterEach((to, from) => {
+    if(from.path !== to.path) {
+      reportEvent('$pageview', {
+        $referrer: getOrigin() + from.path + `${queryTransform2UrlParams(from.query)}`,
+      })
+    }
+  })
+  
+}
